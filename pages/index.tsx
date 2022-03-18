697af1265bf675684c6eec37559d3e39ddd40be4
kherma <kamil.herma01@gmail.com>
2022-03-18 15:25:10 +0100

    feat: update vscode settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className='text-red-500'>Hello Next</h1>
+      <h1 className="text-red-500">Hello Next</h1>
     </div>
   );
 };
