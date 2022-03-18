9cff7d827e4957387f70e8b7242b7c1bea797d64
kherma <kamil.herma01@gmail.com>
2022-03-18 15:23:15 +0100

    feat: update eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Hello Next</h1>
+      <h1 className='text-red-500'>Hello Next</h1>
     </div>
   );
 };
